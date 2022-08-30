8f62ff90487e0e5f8d9f00d3ceaad4ef82501467
trokeibunor <okeibunoremma@gmail.com>
2022-08-30 10:14:31 +0100

    added Skills section

--- src/components/SkillsSect.vue
+++ src/components/SkillsSect.vue
@@ -5,44 +5,68 @@
       <p>SKILLS</p>
     </div>
     <div class="control-tab">
-      <div id="design">
+      <div
+        id="design"
+        :class="{ active: clickedBox == 'design' }"
+        @click="clickedBox = 'design'"
+      >
         <img src="../components/icons/design-icon.svg" alt="" srcset="" />
         <p>DESIGN</p>
-        <img :src="caratDown" alt="" srcset="" />
+        <img
+          :src="clickedBox == 'design' ? caratUp : caratDown"
+          alt=""
+          srcset=""
+        />
       </div>
-      <div id="frontend">
+      <div
+        id="frontend"
+        :class="{ active: clickedBox == 'frontend' }"
+        @click="clickedBox = 'frontend'"
+      >
         <img src="../components/icons/frontend-icon.svg" alt="" srcset="" />
         <p>FRONTEND</p>
-        <img :src="caratUp" alt="" srcset="" />
+        <img
+          :src="clickedBox == 'frontend' ? caratUp : caratDown"
+          alt=""
+          srcset=""
+        />
       </div>
-      <div id="backend">
+      <div
+        id="backend"
+        :class="{ active: clickedBox == 'backend' }"
+        @click="clickedBox = 'backend'"
+      >
         <img src="../components/icons/backend-icon.svg" alt="" srcset="" />
         <p>BACKEND</p>
-        <img :src="caratDown" alt="" srcset="" />
+        <img
+          :src="clickedBox == 'backend' ? caratUp : caratDown"
+          alt=""
+          srcset=""
+        />
       </div>
     </div>
     <!-- Skill box -->
     <div class="skill-box">
-      <!-- <div class="design-box">
-        <ProgressBar title="Figma" width="70"/>
-        <ProgressBar title="Sketch" width="60"/>
-        <ProgressBar title="Canva" width="80"/>
-      </div> -->
-      <div class="frontend-box">
+      <div v-if="clickedBox == 'design'" class="design-box">
+        <ProgressBar title="Figma" width="70" />
+        <ProgressBar title="Sketch" width="60" />
+        <ProgressBar title="Canva" width="80" />
+      </div>
+      <div v-if="clickedBox == 'frontend'" class="frontend-box">
         <ProgressBar title="HTML" width="80" />
-        <ProgressBar title="CSS, SCSS" width="90"/>
-        <ProgressBar title="tailwindCSS, Bootstrap" width="80"/>
+        <ProgressBar title="CSS, SCSS" width="90" />
+        <ProgressBar title="tailwindCSS, Bootstrap" width="80" />
         <ProgressBar title="Javascript" width="90" />
         <ProgressBar title="React" width="60" />
         <ProgressBar title="Vue" width="90" />
       </div>
-      <!-- <div class="backend-box">
+      <div v-if="clickedBox == 'backend'" class="backend-box">
         <ProgressBar title="Nodejs/Express" width="80" />
         <ProgressBar title="PHP" width="70" />
         <ProgressBar title="Python" width="60" />
         <ProgressBar title="MongoDB" width="80" />
         <ProgressBar title="PostMan" width="70" />
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -52,9 +76,14 @@ import { ref } from "vue";
 import ProgressBar from "./ProgressBar.vue";
 const caratDown = ref("src/components/icons/carat-down.svg");
 const caratUp = ref("src/components/icons/carat-up.svg");
+const clickedBox = ref("frontend");
 </script>
 
 <style lang="scss" scoped>
+// dynamic classes
+.active {
+  color: #4415ff;
+}
 #skills {
   display: flex;
   flex-direction: column;
